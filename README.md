8b1b3684f069d3ba60bcdc3496ac9a9072eecf80
Stéphan Zych <stephan@monkemonk.be>
2012-12-15 00:04:52 +0100

    Refactor

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-jQuery.notifications Plugin - Beta
+jQuery.notifications Plugin - v 1.0
 ==================
 
 Notifications System inspired by Growl
@@ -10,7 +10,7 @@ Notifications System inspired by Growl
 
 ``` javascript
 $(document).ready(function() {
-	$.notifications({text: 'Lorem ipdum dolor sit amet...'});
+    $.notifications({content: '<b>Lorem ipdum</b> dolor sit amet...'});
 });
 ```
 
@@ -18,19 +18,48 @@ $(document).ready(function() {
 
 ``` javascript
 {
-  className: 'required',                  // (default) 
-  
-  // Callbacks
-  callback: function(settings, valid) {   // callback 
-    // do something
-  }
+    className: 'notification',
+
+    alive: 4000,
+    fadeIn: 600,
+    fadeOut: 800,
+    sticky: false,
+
+    // Template
+    tpl: '<div class="{className}"><div class="{className}-img"><img src="{image}" alt="" /></div><div class="{className}-content"><div class="{className}-title">{title}</div>{content}</div></div>',
+
+    // Callback
+    onShow: function () {},
+    onHide: function () {}
 }
 ```
 
 ### Methods
 
+#### `add` ($.notification('add', params) is the same as $.notifications(params))
+
+jQuery.notifications use a tiny template system that let you an easy way to customize your notifications.
 
 ``` javascript
+$.notifications('add', {
+    title: 'Lorem ipsum',
+    content: '<b>Lorem ipsum</b> dolor sit amet...',
+
+    className: 'simple',
+
+    tpl: '<div class="{className}"><h1>{title}</h1><p>{content}</p></div>'
+});
+
+```
+
+#### `remove`
+
+``` javascript
+$.notifications('remove');
+
+// or
+
+$.notifications('remove', identifier);
 
 ```
 
--- jquery.notifications.js
+++ jquery.notifications.js
@@ -6,151 +6,156 @@
  */
 
 (function ($) {
-	"use strict";
-	
-	var defaults = {
-		className: 'notification',
-		alive: 4000,
-		fadeIn: 600,
-		fadeOut: 800,
-		sticky: false,
-		
-		// Template
-		tpl: '<div class="{className}">{image}<div class="{className}-title">{title}</div><div class="{className}-text">{text}</div></div>',
-		
-		image: '',
-		title: '',
-		text: '',
-		
-		// Callback
-		callback: function () {}
-	}, settings = {};
-	
-	function render(template, data) {
-		for (var k in data) {
-			template = template.replace(new RegExp('{' + k + '}', 'g'), data[k]);
-		}
-		
-		return template;
-	} // render
-	
-	var methods = {
-		// ========================================================================
-		add: function (options) {
-			settings = $.extend({}, defaults, options);
-			
-			return this.each(function () {
-				
-			});
-		}, // init
-		
-		// ========================================================================
-		remove: function () {
-			
-		} // methodName
-	};
-	
-	$.notifications = function (options) {
-		
-		if (methods[options]) {
-			return methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof options === 'object' || !options) {
-			return methods.add.apply(this, arguments);
-		} else {
-			$.error('Method "' + options + '" does not exist in $.notifications plugin!');
-		}
-		
-	}; // $.fn.
-	
-})(jQuery); // jQuery.() by Stéphan Zych (monkeymonk.be)
+    "use strict";
+    
+    var render = function (template, data) {
+        for (var k in data) {
+            template = template.replace(new RegExp('{' + k + '}', 'g'), data[k]);
+        }
+        
+        return template;
+    }; // render
 
+    var notifications = (function () {
+        var defaults = {
+            className: 'notification',
 
+            alive: 4000,
+            fadeIn: 600,
+            fadeOut: 800,
+            sticky: false,
 
+            // Template
+            tpl: '<div class="{className}"><div class="{className}-img"><img src="{image}" alt="" /></div><div class="{className}-content"><div class="{className}-title">{title}</div>{content}</div></div>',
 
+            // Callback
+            onShow: function () {},
+            onHide: function () {}
+        }; // defaults
 
-;(function($){
-	$.notifications = {
-		defaults: {
-			className: 'notification'
-			, alive: 5000
-			, fadeIn: 600
-			, fadeOut: 800
-			, tpl: '<div class="{className}">{image}<div class="{className}-title">{title}</div><div class="{className}-text">{text}</div></div>'
-			, callback: function(){}
-			
-			, sticky: false
-			
-			, image: ''
-			, title: ''
-			, text: ''
-		}, // defaults
-		
-		private: {
-			isSet: false
-			, count: 0
-		}, // private
-		
-		add: function(params){
-			var o = this, private = this.private
-			
-			s = $.extend({}, this.defaults, params)
-			
-			s.image = s.image ? '<div class="' + s.className + '-img"><img src="' + s.image + '" alt="" /></div>' : ''
-			
-			if(!private.isSet){
-				$('body').append('<div class="' + s.className + '-wrapper"></div>')
-				private.isSet = true
-			}
-			
-			private.count++
-			
-			$(o.tpl(s.tpl, s)).attr('data-notification', private.count)
-			.appendTo('.' + s.className + '-wrapper').css({opacity: 0})
-			.animate({opacity: 1}, s.fadeIn, function(){
-				var n = $(this).attr('data-notification')
-				
-				$(this).click(function(){o.remove.call(o, n, s)})
-				$(this).find('a').click(function(e){e.stopImmediatePropagation()})
-				
-				if(!s.sticky){
-					var timer = setTimeout(function(){o.remove.call(o, n, s)}, s.alive)
-					$(this).bind({
-						mouseenter: function(){
-							clearTimeout(timer)
-						}
-						, mouseleave: function(){
-							timer = setTimeout(function(){o.remove.call(o, n, s)}, s.alive)
-						}
-					})
-				}
-				
-				if(typeof s.callback == 'function')	s.callback.call(o, s)
-			})
-		}, // add
-		
-		remove: function(n, s){
-			var o = this, s = s || o.defaults, private = o.private
-			
-			$('.' + s.className + '[data-notification="' + n + '"]')
-			.animate({opacity: 0}, s.fadeOut, function(){
-				$(this).animate({height: 0}, 150, function(){
-					$(this).remove()
-				})
-			})
-		}, // remove
-		
-		removeAll: function(params){
-			$('.' + s.className)
-			.each(function(){
-				$(this).animate({opacity: 0}, s.fadeOut, function(){
-					$(this).animate({height: 0}, 150, function(){$(this).remove()})
-				})
-			})
-		}, // removeAll
-		
-		tpl: function(s, d){
-			for(var k in d)	s = s.replace(new RegExp('{' + k + '}', 'g'), d[k])
-			return s
-		} // tpl
-	}
-})(jQuery) // jQuery.notifications by Stéphan Zych (monkeymonk.be)
+        var add = function (oParams) {
+            if (!$('#notifications-wrapper').length) {
+                $('body').append('<div id="notifications-wrapper"></div>');
+            }
+
+            var $wrapper = $('#notifications-wrapper'),
+                count = $wrapper.data('count') + 1 || 1;
+
+            defaults = $.extend({}, defaults, oParams);
+
+            $wrapper.data('count', count);
+
+            $(render(defaults.tpl, defaults))
+            .attr('data-id', count)
+            .appendTo($wrapper).css({
+                opacity: 0
+            })
+            .animate({
+                opacity: 1
+            }, defaults.fadeIn, function () {
+                var self = this, 
+                    $self = $(self), 
+                    iId = parseInt(self.getAttribute('data-id'), 10);
+
+                $self
+                .on('click', function () {
+                    $.notifications('remove', iId);
+                })
+                .find('a')
+                .on('click', function (e) {
+                    e.stopImmediatePropagation();
+                });
+
+                if (!defaults.sticky) {
+                    $self.data('timer', setTimeout(function () {
+                        $.notifications('remove', iId);
+                    }, defaults.alive));
+
+                    $self
+                    .on('mouseenter', function () {
+                        clearTimeout($self.data('timer'));
+                    })
+                    .on('mouseleave', function () {
+                        $self.data('timer', setTimeout(function () {
+                            $.notifications('remove', iId);
+                        }, defaults.alive));
+                    });
+                }
+
+                defaults.onShow(defaults);
+            });
+        }; // add
+
+        var remove = function () {
+            var iId = $('.notification:not(.removing)').last().data('id'),
+                $notification, fCallback;
+
+            if (arguments.length) {
+                if (typeof arguments[0] === 'function') {
+                    fCallback = arguments[0];
+                } else if (typeof arguments[1] === 'function') {
+                    iId = arguments[0];
+
+                    fCallback = arguments[1];
+                } else {
+                    iId = arguments[0];
+                }
+            }
+
+            $notification = $('[data-id="' + iId + '"]');
+
+            if (fCallback) {
+                defaults = $.extend({}, defaults, {onHide: fCallback});
+            }
+            
+            $notification
+            .addClass('removing')
+            .animate({
+                opacity: 0
+            }, defaults.fadeOut, function () {
+                $notification.animate({
+                    height: 0
+                }, 150, function () {
+                    clearTimeout($notification.data('timer'));
+
+                    $notification.remove();
+
+                    defaults.onHide(defaults);
+                });
+            });
+        }; // remove
+
+        var removeAll = function (fCallback) {
+            if (typeof fCallback === 'function') {
+                defaults = $.extend({}, defaults, {onHide: fCallback});
+            }
+
+            $('.notification')
+            .each(function () {
+                $.notifications('remove', this.getAttribute('data-id'), {onHide: null});
+            });
+
+            $('#notifications-wrapper').data('count', 0);
+
+            defaults.onHide(defaults);
+        }; // removeAll
+
+        return {
+            add: add,
+            remove: remove,
+            removeAll: removeAll
+        };
+    } ()); // notifications
+
+    
+    $.notifications = function () {
+        if (typeof arguments[0] === 'string') {
+            return notifications[arguments[0]].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof arguments[0] === 'object' || !arguments) {
+            return notifications.add.apply(this, arguments);
+        } else {
+            $.error('Method "' + arguments[0] + '" does not exist in $.notifications plugin!');
+        }
+    }; // $.notifications
+    
+})(jQuery); // jQuery.notifications() by Stéphan Zych (monkeymonk.be)
